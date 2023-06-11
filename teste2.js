701f557d172d8e7fa913c08c39bde2eecf433bc2
Raphael Borba <raphaelbsx@gmail.com>
2023-06-11 13:33:37 -0300

    feat: add id when create a new user

--- teste2.js
+++ teste2.js
@@ -8,14 +8,20 @@ function isString(x) {
 module.exports = function (req, res) {
 
     var { name, job } = req.body;
+    let id
 
     if (!isString(name) || !isString(job)) return res.status(400).send('Formato incorreto!')
 
     if (name.length < 3 || job.length < 3) return res.status(400).send('Nome e Trabalho precisam ter tamanho maior ou igual a 3!')
 
+    for (let i in data) {
+        id = data[i].id
+    }
+
     var newUser = {
+        id: id + 1,
         name: name,
-        job: job,
+        job: job
     }
 
     data.push(newUser)
