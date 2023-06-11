ccf13d45c1b4c52efa1e494a18a97da2f2fefb03
Raphael Borba <raphaelbsx@gmail.com>
2023-06-11 13:17:35 -0300

    fix: add validation on post user route

--- teste2.js
+++ teste2.js
@@ -1,17 +1,25 @@
-var data =  require("./fakeData");
+var data = require("./fakeData");
 
-module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var jov =  req.body.job;
-    
+function isString(x) {
+
+    return Object.prototype.toString.call(x) === "[object String]"
+}
+
+module.exports = function (req, res) {
+
+    var { name, job } = req.body;
+
+    if (!isString(name) || !isString(job)) return res.status(400).send('Formato incorreto!')
+
+    if (name.length < 3 || job.length < 3) return res.status(400).send('Nome e Trabalho precisam ter tamanho maior ou igual a 3!')
+
     var newUser = {
         name: name,
         job: job,
     }
 
     data.push(newUser)
-    
+
     res.send(newUser);
 
 };
