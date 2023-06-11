ee458976b359c9bc9ebe63a7ef2259c338222f93
Raphael Borba <raphaelbsx@gmail.com>
2023-06-11 13:28:47 -0300

    Fix test 1, return not found case there isn't name

--- teste1.js
+++ teste1.js
@@ -3,12 +3,16 @@ var data =  require("./fakeData");
 const getUser = ( req, res, next ) => {
     
     var name =  req.query.name;
+    
 
     for(let i = 0; i < data.length;  i++) {
-        if(i.name == name) {
-            res.send(data[i]);
+        
+        if(data[i].name == name) {
+            return res.send(data[i]);
         }
     }
+
+    return res.status(404).send('Usuário não encontrado!')
 
 };
 
